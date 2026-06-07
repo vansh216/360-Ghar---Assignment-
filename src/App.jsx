609743119f021b2ad685eb84f8ed51567422c82d
vansh216 <216386028+vansh216@users.noreply.github.com>
2026-06-07 08:59:09 +0530

    add the Navbar , footer and Hero section in the App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import Hero from './components/Hero'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     360 ghar
+    <Navbar/>
+    <Hero />
+    <Footer/>
     </>
   )
 }
